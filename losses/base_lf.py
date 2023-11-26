90b6c8e491c766afca80c9c33fb4f3c2c902f535
imranskiy11 <imranskiy11@gmail.com>
2023-11-26 17:31:25 +0300

    add base error function

--- losses/base_lf.py
+++ losses/base_lf.py
@@ -1,30 +1,65 @@
 import numpy as np
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
+from typing import List
 
 
-def error_function(time_series1, time_series2):
+# Function to calculate mean squared error (MSE) between two time series
+def error_function(
+        time_series1: np.ndarray, time_series2: np.ndarray) -> float:
+    """
+    Calculates the mean squared error (MSE) between two time series.
+
+    Args:
+    - time_series1 (np.ndarray): First time series data.
+    - time_series2 (np.ndarray): Second time series data.
+
+    Returns:
+    - float: Mean squared error between the time series.
+    """
     if len(time_series1) != len(time_series2):
-        raise ValueError("Длины временных рядов должны быть одинаковыми")
+        raise ValueError("Lengths of time series should be the same")
 
-    # Рассчитываем среднеквадратичную ошибку (MSE)
     mse = np.mean((time_series1 - time_series2) ** 2)
     return mse
 
 
-def correlation_error(time_series1, time_series2):
-    if len(time_series1) != len(time_series2):
-        raise ValueError("Длины временных рядов должны быть одинаковыми")
+# Function to calculate Pearson correlation-based error between two time series
+def correlation_error(
+        time_series1: np.ndarray, time_series2: np.ndarray) -> float:
+    """
+    Calculates the error based on Pearson correlation
+    coefficient between two time series.
 
-    # Вычисляем корреляцию Пирсона между временными рядами
+    Args:
+    - time_series1 (np.ndarray): First time series data.
+    - time_series2 (np.ndarray): Second time series data.
+
+    Returns:
+    - float: Error based on Pearson correlation
+    coefficient between the time series.
+    """
+    if len(time_series1) != len(time_series2):
+        raise ValueError("Lengths of time series should be the same")
+
     correlation = np.corrcoef(time_series1, time_series2)[0, 1]
-    # Переводим корреляцию в ошибку:
-    # 1 - корреляция (чем выше корреляция, тем меньше ошибка)
     error = 1 - correlation
     return error
 
 
-def dtw_error(time_series1, time_series2):
-    # Вычисляем расстояние DTW между временными рядами
+# Function to calculate Dynamic Time Warping (DTW) distance
+# between two time series
+def dtw_error(
+        time_series1: List[float], time_series2: List[float]) -> float:
+    """
+    Calculates the Dynamic Time Warping (DTW) distance between two time series.
+
+    Args:
+    - time_series1 (List[float]): First time series data.
+    - time_series2 (List[float]): Second time series data.
+
+    Returns:
+    - float: Dynamic Time Warping (DTW) distance between the time series.
+    """
     distance, _ = fastdtw(time_series1, time_series2, dist=euclidean)
     return distance
